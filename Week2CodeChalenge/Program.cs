67dda83154357073ec8b3893a2406589eec5fbb8
Zach Urbach <urbach-zach@gmail.com>
2014-06-27 09:53:45 -0600

    initial check in

---
+++ Week2CodeChalenge/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2CodeChalenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 21; i++)
+            {
+                FizzBuzz(i);
+            }
+
+            Console.WriteLine();
+            LetterCounter('i', "I love biscuits and gravy. It's the best brekfast ever.");
+            LetterCounter('n', "Never gonna give you up. Never gonna let you down.");
+            LetterCounter('s', "Sally sells seashells down by the seashore. She's from Sussex.");
+
+            Console.WriteLine();
+            TextStats("The monkey jumped over the dog. A cat is being lazy and fat. That cloud is shaped like a spaceship.");
+
+            Console.ReadKey();
+        }
+
+        static void FizzBuzz(int number)
+        {
+            if (number % 5 == 0 && number % 3 == 0)
+            {
+                Console.WriteLine("Fizz Buzz");
+            } 
+            else if (number % 5 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (number % 3 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(number);
+            }
+        }
+        static void LetterCounter(char letter, string inString)
+        {
+            
+            int lowCount = 0;
+            int upCount = 0;
+
+            for (int i = 0; i < inString.Length; i++)
+            {
+                if (inString[i] == (char.ToLower(letter)))
+                {
+                    lowCount++;
+                }
+                else if (inString[i] == (char.ToUpper(letter)))
+                {
+                    upCount++;
+                }
+            }
+             int total = lowCount + upCount;
+
+            
+            Console.WriteLine("Input: " + inString +
+                "\n" + "Number of lowercase " + letter + "'s found: " + lowCount +
+                "\n" + "Number of UPPERCASE " + letter + "'s found: " + upCount +
+                "\n" + "Total Number of " + letter + "'s found: " + total);
+        }
+        static void TextStats(string input)
+        {
+            Console.WriteLine("Original input; " + input);
+
+            // number of characters  it can be int numberOfCaracters = input.Length;
+            Console.WriteLine("Number of caracters: " + input.Length);
+            //number of words
+            Console.WriteLine("# of words: " + input.Split(' ').Length);
+
+            int numVowels = 0;
+            int numCons = 0;
+            int specChars = 0;
+            for (int i = 0; i < input.Length; i++)
+            
+
+            {
+                
+                //grrabing one letter from the string
+                // by it's index
+                char letter = input[i];
+                //number of vowels
+                if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')
+                {
+                    //found a vowel! add 1 to the vowel count
+                    numVowels = numVowels + 1;
+                }
+                else if (letter == '.' || letter == ' ' || letter == ',')
+                {
+                    specChars = specChars + 1;
+                }
+                //number of constinants
+                else
+                {
+                    numCons = numCons + 1;
+                }
+                //number of special characters
+            }
+
+            //output number of vowels, cons, 
+            // and 
+            Console.WriteLine("# of vowels: " + numVowels);
+            Console.WriteLine("# of consonants: " + numCons);
+            Console.WriteLine("# of special characters: " + specChars);
+            Console.WriteLine("Longest word: ");
+            Console.WriteLine("Shortest word: ");
+        }
+    }
+}
